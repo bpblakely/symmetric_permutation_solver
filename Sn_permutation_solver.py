ccc4e5546c676034b868cef7c12b635009e85825
Brian Blakely <58829420+bpblakely@users.noreply.github.com>
2020-11-13 23:24:59 -0500

    Update Sn_permutation_solver.py

--- Sn_permutation_solver.py
+++ Sn_permutation_solver.py
@@ -1,7 +1,7 @@
 # Solves common problems for the Symmetric Group Sn when given a permutation of Sn (https://en.wikipedia.org/wiki/Symmetric_group)
 
 # Author: Brian Blakely | github: https://github.com/bpblakely
-# Theorem's based on content in Contemporary Abstract Algebra SEVENTH EDITION by Joseph A. Gallian
+# Theorems based on Contemporary Abstract Algebra SEVENTH EDITION by Joseph A. Gallian
 
 # Requires Either CPython/PyPy 3.6, Python 3.7, or higher
 
